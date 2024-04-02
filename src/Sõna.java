10bd05e44cb7568d7dec6511b1d69be457b7b40b
MarttisLadev <145854002+MarttisLadev@users.noreply.github.com>
2024-04-02 12:37:37 +0300

    Sõna.java

--- src/Sõna.java
+++ src/Sõna.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -6,79 +9,106 @@ public class Sõna {
 
     private String suvalineSõna;
     private char[] arvatudTähed;
-    private int arvatudTähtedeArv;
     private Set<Character> kasutatudTähed;
 
     public Sõna(String suvalineSõna) {
-        this.suvalineSõna = suvalineSõna;
+        this.suvalineSõna = suvalineSõna.toUpperCase(); // Teisendame kogu sõna suurtähtedeks
         this.arvatudTähed = new char[suvalineSõna.length()];
 
-        // Esimene täht antakse ette, ülejäänud tähtede asemel kuvatakse alakriipsud
-        arvatudTähed[0] = suvalineSõna.charAt(0);
-        for (int i = 1; i < suvalineSõna.length(); i++) {
-            arvatudTähed[i] = '_';
+        for (int i = 0; i < suvalineSõna.length(); i++) {
+            if (i == 0) {
+                arvatudTähed[i] = Character.toUpperCase(suvalineSõna.charAt(i)); // Esimene täht suurtäheks
+            } else {
+                arvatudTähed[i] = '_'; // Kõik teised tähed "_" tähed
+            }
         }
-        this.arvatudTähtedeArv = 1;
 
         kasutatudTähed = new HashSet<>();
     }
 
     public void arvaTäht(char täht) {
-        if (täht == suvalineSõna.charAt(arvatudTähtedeArv)) {
-            arvatudTähed[arvatudTähtedeArv] = täht;
-            arvatudTähtedeArv++;
-            System.out.println("Õige! " + String.valueOf(arvatudTähed));
+        täht = Character.toUpperCase(täht); // Konverteerime sisestatud tähe suurtäheks
+        boolean õige = false;
+        for (int i = 0; i < suvalineSõna.length(); i++) {
+            if (Character.toUpperCase(suvalineSõna.charAt(i)) == täht && arvatudTähed[i] == '_') {
+                arvatudTähed[i] = täht; // Kui sisestatud täht on sõnas ja koht on tühi, siis asendame "_" tähega
+                õige = true;
+            }
+        }
+        if (õige) {
+            if (onÕigeSõnaArvatud()) {
+                System.out.println("Õige! " + suvalineSõna);
+                System.exit(0);
+            } else {
+                System.out.println("Õige! " + String.valueOf(arvatudTähed));
+            }
         } else {
             System.out.println("Vale!");
         }
     }
 
+    public boolean tähtSaadaval(char täht) {
+        täht = Character.toUpperCase(täht);
+        return kasutatudTähed.contains(täht);
+    }
+
+    public void lisaTäht(char täht) {
+        täht = Character.toUpperCase(täht);
+        kasutatudTähed.add(täht);
+    }
+
+    public boolean onÕigeSõnaArvatud() {
+        for (char täht : arvatudTähed) {
+            if (täht == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void arvaSõna() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Esimene täht: " + arvatudTähed[0]);
-        for (int i = 1; i < suvalineSõna.length(); i++) {
-            System.out.print("_");
+        System.out.print("Alguses: ");
+        for (int i = 0; i < suvalineSõna.length(); i++) {
+            System.out.print(arvatudTähed[i]);
         }
         System.out.println();
-        System.out.println("Arva täht:");
 
-        while (arvatudTähtedeArv < suvalineSõna.length()) {
-            char sisestatudTäht = scanner.next().charAt(0);
+        while (true) {
+            System.out.println("Arva täht või sisesta 'lõpeta' sõna lõpetamiseks:");
+            String sisend = scanner.nextLine();
+            if (sisend.equalsIgnoreCase("lõpeta")) {
+                break;
+            }
+            if (sisend.length() != 1) {
+                System.out.println("Palun sisesta ainult üks täht!");
+                continue;
+            }
+            char sisestatudTäht = sisend.charAt(0);
+            sisestatudTäht = Character.toUpperCase(sisestatudTäht); // Teisendame sisestatud tähe suurtäheks
+            if (tähtSaadaval(sisestatudTäht)) {
+                System.out.println("See täht on juba kasutatud!");
+                continue;
+            }
             arvaTäht(sisestatudTäht);
+            lisaTäht(sisestatudTäht);
+            prindiKasutatudTähed();
         }
 
-        System.out.println("Edukalt lahendatud. Sõnaks oli: " + suvalineSõna);
+        System.out.println("Edukalt lõpetatud. Sõnaks oli: " + suvalineSõna);
 
         scanner.close();
     }
 
-    public boolean tähtSaadaval(char täht) {
-        // kui täht on juba mängija poolt pakutud, siis seda ei saa enam pakkuda
-        if (kasutatudTähed.contains(täht))
-            return false; // täht on juba kasutuses, ei saa enam kasutada
-        else
-            return true;
-    }
-
-    public void lisaTäht(char täht) {
-        // kui täht pakutakse ja see on saadaval
-        kasutatudTähed.add(täht);
-    }
-
     public void prindiKasutatudTähed() {
-        if (!kasutatudTähed.isEmpty()) // kui on midagi printida
-        {// kõikide kasutadu tähtede väljastamine eraldatult tühikuga
-            System.out.print("Kasutatud tähed: ");
-            kasutatudTähed.forEach(täht -> System.out.print(täht + " "));
-            System.out.println();
+        System.out.print("Kasutatud tähed: ");
+        for (char c : kasutatudTähed) {
+            System.out.print(c + " ");
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        String suvalineSõna = FailiLugeja.loeSuvalineRida();
-        String[] osad = suvalineSõna.split("\\s+");
-        Sõna mängitavSõna = new Sõna(osad[0]);
-        mängitavSõna.arvaSõna();
+        System.out.println();
     }
 }
+
+
+
