8d4bd836f6b02636ef7c78c299cfb91f80800073
MarttisLadev <145854002+MarttisLadev@users.noreply.github.com>
2024-04-02 17:09:03 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,39 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        /*String suvalineSõna;
-        try {
-            suvalineSõna = FailiLugeja.eraldaSõna(FailiLugeja.loeSuvalineRida()).toUpperCase(); // Teisendame loetud sõna suurtähtedeks
-            Sõna mängitavSõna = new Sõna(suvalineSõna);
-            mängitavSõna.arvaSõna();
-        } catch (Exception e) {
-            System.out.println("Viga faili lugemisel: " + e.getMessage());
-        }*/
-
-        // mängija isendi loomine
         Mängija mängija = new Mängija();
+        Set<Character> kasutatudTähed = new HashSet<>(); // Lisatud kasutatud tähtede hoidmiseks
 
-        // sõna lugemine
         String rida = FailiLugeja.loeSuvalineRida();
         String sõna = FailiLugeja.eraldaSõna(rida);
 
-        Sõna arvatavSõna = new Sõna(sõna); // sõna isend - iga sõna kohta uus isend
+        Sõna arvatavSõna = new Sõna(sõna);
 
         Scanner scanner = new Scanner(System.in);
 
+        while (mängija.getElud() > 0) {
+            arvatavSõna.prindiArvatudTähed();
 
-        // nii kaua, kuni sõna ära arvatakse või elud otsa saavad
-        while (mängija.getElud() > 0) {// sisendi küsimine
-            arvatavSõna.prindiKasutatudTähed();
-
-            char täht; // täht mida pakutakse
+            char täht;
             while (true) {
-
-                // kontrollib, kas sisend on korrektne
                 System.out.print("Pakkuge täht: ");
-                String pakutud = scanner.nextLine(); // kasutaja sisend
+                String pakutud = scanner.nextLine();
                 täht = Character.toUpperCase(pakutud.charAt(0));
-                if (pakutud.length() == 1 && arvatavSõna.tähtSaadaval(täht)) {
-                    // sisend on korrektne
-                    // st tähte ei ole enne pakutud ja sisendi pikkus on 1
+                if (pakutud.length() == 1) {
                     System.out.println("See on korrektne sisend");
-                    arvatavSõna.lisaTäht(täht);
-                    break; // kui programm on saanud korrektse sisendi
+                    kasutatudTähed.add(täht); // Lisame kasutatud tähe hulka
+                    break;
                 } else {
                     System.out.println("Ebakorrektne sisend.");
                 }
             }
 
-            // Sõna klassis kontrollitakse, kas see täht kuulub sõnasse
-            // boolean kuulumine = arvatavSõna.kuulub(täht)
-            boolean kuulumine = false;
+            boolean kuulumine = arvatavSõna.arvaTäht(täht);
             if (kuulumine) {
-                // sõna.avaTäht(täht)
-                // sõna.väljastaSõna()
                 mängija.lisaPunkte(100);
             } else {
                 System.out.println("Pakutud täht ei kuulu sõnasse");
